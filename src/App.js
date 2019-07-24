f230f5441530365ed02d3dda6475ee7d9e0978c4
Furtive Pygmy <rzvgib001@gmail.com>
2019-07-25 02:45:12 +0400

    Setting up sign in form

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
+import SignInPage from './pages/auth/auth.component';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/sign-in" component={SignInPage} />
       </Switch>
     </div>
   );
